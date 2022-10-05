b84836d2d6417ad7fafbaa8b682790d825a950d7
Maxence Guilbot <guilbotmax@hotmail.fr>
2022-10-05 15:05:17 +0100

    Nested objects.

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,9 @@
 const aCar = {
-  owner : "Joe Bloggs",
+  owner: "Joe Bloggs",
+  previous_owners: [
+    {name : ' Pat Smith', address : '1 Main Street'},
+    {name : 'Sheila Dwyer', address :  '2 High Street'}
+],
   type : {
     make : 'Toyota',
     model : 'Corolla',
@@ -22,3 +26,9 @@ aCar.color = {
 }
 
 console.log('It is a ' + aCar.color.exterior + ' car, ' + aCar.color.interior.texture + ' mileage, with ' + aCar.color.interior.shade + ' interior ...');
+
+console.log('First owner : ' + aCar.previous_owners[0] );
+
+console.log(
+  "First owner: " +
+    aCar.previous_owners[0].name + ' - ' + aCar.previous_owners[0].address );
